b75e3980086b866c8afd749b6f00a02dece88ec2
Aryansingh1909 <139946427+Aryansingh1909@users.noreply.github.com>
2025-04-14 18:14:54 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://mychat-k4wp.onrender.com/api" : "/api",
+  baseURL: "https://mychat-k4wp.onrender.com/api" ,
   withCredentials: true,
 });
